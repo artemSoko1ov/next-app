28bcc4ede5162991b5fb63ef504aac1672cbe2e4
Artem Sokolov <the.jelll.2006@gmail.com>
2025-10-21 17:42:48 +0500

    fixed: redux

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import ReduxProvider from "@/store/ReduxProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
       <Header />
+      <ReduxProvider>
         {children}
+      </ReduxProvider>
       </body>
     </html>
   );
---
+++ store/slices/usersSlice.ts
@@ -0,0 +1,33 @@
+
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+interface User {
+  id: number
+  name: string
+  email: string
+  phone: string
+}
+
+interface UsersState {
+  list: User[]
+}
+
+const initialState: UsersState = {
+  list: [],
+}
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {
+    setUsers: (state, action: PayloadAction<User[]>) => {
+      state.list = action.payload
+    },
+    addUser: (state, action: PayloadAction<User>) => {
+      state.list.push(action.payload)
+    },
+  },
+})
+
+export const { setUsers, addUser } = usersSlice.actions
+export default usersSlice.reducer
